7d3e87ea55687c76f90637f0a89de55d23300a6f
omrobbie <putu.roby@yahoo.com>
2017-12-25 11:35:42 +0800

    parsing data from first to second activity

--- IntentApplication/app/src/main/java/com/omrobbie/intentapplication/MainActivity.java
+++ IntentApplication/app/src/main/java/com/omrobbie/intentapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.omrobbie.intentapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        final EditText editText = (EditText) findViewById(R.id.edit_first);
+        String userMessage = editText.getText().toString();
+
+        intent.putExtra("firstMessage", userMessage);
+        startActivity(intent);
     }
 }
--- IntentApplication/app/src/main/java/com/omrobbie/intentapplication/SecondActivity.java
+++ IntentApplication/app/src/main/java/com/omrobbie/intentapplication/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.omrobbie.intentapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle firstData = getIntent().getExtras();
+        if (firstData == null) return;
+
+        String firstMessage = firstData.getString("firstMessage");
+
+        TextView textView = (TextView) findViewById(R.id.text_data);
+        textView.setText(firstMessage);
+    }
+
+    public void onClick(View view) {
+        finish();
     }
 }
