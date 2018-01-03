f1455e2037a6187ee385eb79d3e1793b0f616699
omrobbie <putu.roby@yahoo.com>
2018-01-04 03:46:48 +0800

    create send broadcast action

--- SendBroadcast/app/src/main/java/com/omrobbie/sendbroadcast/MainActivity.java
+++ SendBroadcast/app/src/main/java/com/omrobbie/sendbroadcast/MainActivity.java
@@ -1,7 +1,9 @@
 package com.omrobbie.sendbroadcast;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendOutBroadcast(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.omrobbie.sendbroadcast");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
     }
 }
