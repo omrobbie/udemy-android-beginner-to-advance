7edf66de0a91f153a5d0c6c90e436bc861ff67b0
omrobbie <putu.roby@yahoo.com>
2018-01-04 06:42:04 +0800

    display array of string to list view, the simplest way

--- MyListApp/app/src/main/java/com/omrobbie/mylistapp/MainActivity.java
+++ MyListApp/app/src/main/java/com/omrobbie/mylistapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.omrobbie.mylistapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] fruits = {"Apple", "Orange", "Peach", "Mango"};
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fruits);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        myListView.setAdapter(adapter);
     }
 }
