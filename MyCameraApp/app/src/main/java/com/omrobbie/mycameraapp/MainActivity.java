49b7489b98126441ccc5a673946db3ca0453316c
omrobbie <putu.roby@yahoo.com>
2018-01-05 03:09:13 +0800

    setup camera and display in image view

--- MyCameraApp/app/src/main/java/com/omrobbie/mycameraapp/MainActivity.java
+++ MyCameraApp/app/src/main/java/com/omrobbie/mycameraapp/MainActivity.java
@@ -1,13 +1,53 @@
 package com.omrobbie.mycameraapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private Button captureButton;
+    private ImageView myImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupEnv();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+
+            myImageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void launchCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    private void setupEnv() {
+        captureButton = (Button) findViewById(R.id.captureButton);
+        myImageView = (ImageView) findViewById(R.id.myImageView);
+
+        if (!hasCamera()) captureButton.setEnabled(false);
+    }
+
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
 }
