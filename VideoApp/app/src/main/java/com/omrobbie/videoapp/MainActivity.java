192cf446bc1348e33baa2eed21d617b1dc5d4a66
omrobbie <putu.roby@yahoo.com>
2018-01-05 02:49:59 +0800

    set video path and play it

--- VideoApp/app/src/main/java/com/omrobbie/videoapp/MainActivity.java
+++ VideoApp/app/src/main/java/com/omrobbie/videoapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.omrobbie.videoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView myVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupEnv();
+
+        myVideoView.setVideoPath("http://www.ebookfrenzy.com/android_book/movie.mp4");
+        myVideoView.start();
+    }
+
+    private void setupEnv() {
+        myVideoView = (VideoView) findViewById(R.id.myVideoView);
     }
 }
