314e02fa69f4b677acaeee6d0e139615d53de243
omrobbie <putu.roby@yahoo.com>
2018-01-05 01:03:12 +0800

    create database and table

---
+++ MyDataApp/app/src/main/java/com/omrobbie/mydataapp/MyDbHandler.java
@@ -0,0 +1,37 @@
+package com.omrobbie.mydataapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by omrobbie on 05/01/2018.
+ */
+
+public class MyDbHandler extends SQLiteOpenHelper {
+
+    public static final String TABLE_PRODUCT = "products";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_PRODUCT_NAME = "product_name";
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "products.db";
+
+    public MyDbHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "CREATE TABLE " + TABLE_PRODUCT + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_PRODUCT_NAME + " TEXT);";
+
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
