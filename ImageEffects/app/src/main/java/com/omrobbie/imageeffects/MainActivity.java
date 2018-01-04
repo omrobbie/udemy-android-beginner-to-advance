5a98647e29bd1d89af2136eb932de66af31baacc
omrobbie <putu.roby@yahoo.com>
2018-01-05 03:24:44 +0800

    invert image color

--- ImageEffects/app/src/main/java/com/omrobbie/imageeffects/MainActivity.java
+++ ImageEffects/app/src/main/java/com/omrobbie/imageeffects/MainActivity.java
@@ -1,13 +1,58 @@
 package com.omrobbie.imageeffects;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView myImageView;
+
+    private Drawable myDrawable;
+    private Bitmap myBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupEnv();
+    }
+
+    private void setupEnv() {
+        myImageView = (ImageView) findViewById(R.id.myImageView);
+        myDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.firebase, null);
+        myBitmap = ((BitmapDrawable) myDrawable).getBitmap();
+
+        Bitmap newPhoto = invertImage(myBitmap);
+        myImageView.setImageBitmap(newPhoto);
+    }
+
+    private Bitmap invertImage(Bitmap original) {
+        Bitmap finalImage = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
+
+        int A, R, G, B;
+        int pixelColor;
+        int heigth = original.getHeight();
+        int width = original.getWidth();
+
+        for (int y = 0; y < heigth; y++) {
+            for (int x = 0; x < width; x++) {
+                pixelColor = original.getPixel(x, y);
+                A = Color.alpha(pixelColor);
+                R = 255 - Color.red(pixelColor);
+                G = 255 - Color.green(pixelColor);
+                B = 255 - Color.blue(pixelColor);
+
+                finalImage.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        return finalImage;
     }
 }
