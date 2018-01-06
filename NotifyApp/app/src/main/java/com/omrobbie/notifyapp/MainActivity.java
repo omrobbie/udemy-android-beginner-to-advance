d8795890d26b3a132e8fa773fb5f67e5b79e32d2
omrobbie <putu.roby@yahoo.com>
2018-01-07 07:02:14 +0800

    setup notification builder

--- NotifyApp/app/src/main/java/com/omrobbie/notifyapp/MainActivity.java
+++ NotifyApp/app/src/main/java/com/omrobbie/notifyapp/MainActivity.java
@@ -1,13 +1,40 @@
 package com.omrobbie.notifyapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int uniqueID = 40111;
+
+    private NotificationCompat.Builder notificationBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setAutoCancel(true);
+    }
+
+    public void myButtonClick(View view) {
+        notificationBuilder.setSmallIcon(R.drawable.firebase);
+        notificationBuilder.setTicker("This is a thicker!");
+        notificationBuilder.setWhen(System.currentTimeMillis());
+        notificationBuilder.setContentTitle("This is the title");
+        notificationBuilder.setContentText("Here is the text");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(uniqueID, notificationBuilder.build());
     }
 }
