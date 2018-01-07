e4ee5fd0b37133deff6fe424ae5a9923daace9e3
omrobbie <putu.roby@yahoo.com>
2018-01-07 08:50:13 +0800

    show text on the screen

--- test/core/src/com/omrobbie/mygame/MyGdxGame.java
+++ test/core/src/com/omrobbie/mygame/MyGdxGame.java
@@ -2,32 +2,40 @@ package com.omrobbie.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private SpriteBatch spriteBatch;
+    private BitmapFont bitmapFont;
+
+    @Override
+    public void create() {
+        setupEnv();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        spriteBatch.begin();
+        bitmapFont.draw(spriteBatch, "Hello World!", 100, 400);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+        bitmapFont.dispose();
+    }
+
+    private void setupEnv() {
+        spriteBatch = new SpriteBatch();
+        bitmapFont = new BitmapFont();
+        bitmapFont.setColor(Color.BLUE);
+    }
 }
