cf681f1ccfbcd1216ef4fc95ac2c710615821537
omrobbie <putu.roby@yahoo.com>
2018-01-07 08:05:20 +0800

    setup layout

--- SharePref/app/src/main/java/com/omrobbie/sharepref/MainActivity.java
+++ SharePref/app/src/main/java/com/omrobbie/sharepref/MainActivity.java
@@ -2,12 +2,25 @@ package com.omrobbie.sharepref;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameInput, passwordInput;
+    private TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupEnv();
+    }
+
+    private void setupEnv() {
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        myText = (TextView) findViewById(R.id.myText);
     }
 }
