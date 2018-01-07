691f44cda38635f69c0ec954620a00f5d4859829
omrobbie <putu.roby@yahoo.com>
2018-01-07 08:14:00 +0800

    save and display data from shared preferences

--- SharePref/app/src/main/java/com/omrobbie/sharepref/MainActivity.java
+++ SharePref/app/src/main/java/com/omrobbie/sharepref/MainActivity.java
@@ -1,9 +1,13 @@
 package com.omrobbie.sharepref;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupEnv();
+    }
+
+    public void saveInfo(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("username", usernameInput.getText().toString());
+        editor.putString("password", passwordInput.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayData(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+        String password = sharedPreferences.getString("password", "");
+
+        myText.setText(username + " | " + password);
     }
 
     private void setupEnv() {
