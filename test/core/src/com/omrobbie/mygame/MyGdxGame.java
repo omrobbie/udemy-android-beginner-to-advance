69c381db97289c8142a0effb892e27cdfe9dfce3
omrobbie <putu.roby@yahoo.com>
2018-01-07 09:43:16 +0800

    show animation using texture atlas

--- test/core/src/com/omrobbie/mygame/MyGdxGame.java
+++ test/core/src/com/omrobbie/mygame/MyGdxGame.java
@@ -5,16 +5,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 
     private SpriteBatch spriteBatch;
     private BitmapFont bitmapFont;
+
     private Texture texture;
     private Sprite sprite;
+
+    private TextureAtlas textureAtlas;
+    private Animation animation;
+    private float timePassed = 0;
 
     @Override
     public void create() {
@@ -27,8 +35,13 @@ public class MyGdxGame extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         spriteBatch.begin();
+
         sprite.draw(spriteBatch);
         bitmapFont.draw(spriteBatch, "Firebase!", 200, 100);
+
+        timePassed += Gdx.graphics.getDeltaTime();
+        spriteBatch.draw((TextureRegion) animation.getKeyFrame(timePassed, true), 300, 500);
+
         spriteBatch.end();
     }
 
@@ -37,6 +50,7 @@ public class MyGdxGame extends ApplicationAdapter {
         spriteBatch.dispose();
         bitmapFont.dispose();
         texture.dispose();
+        textureAtlas.dispose();
     }
 
     private void setupEnv() {
@@ -47,5 +61,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
         texture = new Texture("firebase.png");
         sprite = new Sprite(texture);
+
+        textureAtlas = new TextureAtlas(Gdx.files.internal("walk.atlas"));
+        animation = new Animation(1 / 3f, textureAtlas.getRegions());
     }
 }
