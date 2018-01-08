54249a93fb06d5eaf83399894061cfc88f2e87a7
omrobbie <putu.roby@yahoo.com>
2018-01-09 06:23:59 +0800

    add sound to the game

--- test/core/src/com/omrobbie/mygame/MyGdxGame.java
+++ test/core/src/com/omrobbie/mygame/MyGdxGame.java
@@ -2,20 +2,19 @@ package com.omrobbie.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 
-public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener {
+public class MyGdxGame extends ApplicationAdapter
+        implements GestureDetector.GestureListener,
+        InputProcessor {
 
     private SpriteBatch spriteBatch;
     /*private BitmapFont bitmapFont;
@@ -30,6 +29,8 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
     private OrthographicCamera camera;
     private Texture texture;
     private Sprite sprite;
+
+    private Sound sound;
 
     @Override
     public void create() {
@@ -63,6 +64,8 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
         textureAtlas.dispose();*/
 
         texture.dispose();
+
+        sound.dispose();
     }
 
     /**
@@ -172,6 +175,105 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 
     }
 
+
+    /**
+     * Called when a key was pressed
+     *
+     * @param keycode one of the constants in {@link Input.Keys}
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    /**
+     * Called when a key was released
+     *
+     * @param keycode one of the constants in {@link Input.Keys}
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    /**
+     * Called when a key was typed
+     *
+     * @param character The character
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    /**
+     * Called when the screen was touched or a mouse button was pressed. The button parameter will be {@link Buttons#LEFT} on iOS.
+     *
+     * @param screenX The x coordinate, origin is in the upper left corner
+     * @param screenY The y coordinate, origin is in the upper left corner
+     * @param pointer the pointer for the event.
+     * @param button  the button
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        long soundId = sound.play();
+        sound.setVolume(soundId, 0.9f);
+
+        return false;
+    }
+
+    /**
+     * Called when a finger was lifted or a mouse button was released. The button parameter will be {@link Buttons#LEFT} on iOS.
+     *
+     * @param screenX
+     * @param screenY
+     * @param pointer the pointer for the event.
+     * @param button  the button   @return whether the input was processed
+     */
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    /**
+     * Called when a finger or the mouse was dragged.
+     *
+     * @param screenX
+     * @param screenY
+     * @param pointer the pointer for the event.  @return whether the input was processed
+     */
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    /**
+     * Called when the mouse was moved without any buttons being pressed. Will not be called on iOS.
+     *
+     * @param screenX
+     * @param screenY
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    /**
+     * Called when the mouse wheel was scrolled. Will not be called on iOS.
+     *
+     * @param amount the scroll amount, -1 or 1 depending on the direction the wheel was scrolled.
+     * @return whether the input was processed.
+     */
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
     private void setupEnv() {
         spriteBatch = new SpriteBatch();
         /*bitmapFont = new BitmapFont();
@@ -190,5 +292,8 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
         sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
 
         Gdx.input.setInputProcessor(new GestureDetector(this));
+
+        Gdx.input.setInputProcessor(this);
+        sound = Gdx.audio.newSound(Gdx.files.internal("sounds/kiss.wav"));
     }
 }
