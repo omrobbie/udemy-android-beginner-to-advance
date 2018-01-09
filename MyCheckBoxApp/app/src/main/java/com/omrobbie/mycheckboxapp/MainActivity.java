7520d1637498ce65d34dd0eab821a358f79395b9
omrobbie <putu.roby@yahoo.com>
2018-01-10 00:25:02 +0800

    setup checkbox in action

--- MyCheckBoxApp/app/src/main/java/com/omrobbie/mycheckboxapp/MainActivity.java
+++ MyCheckBoxApp/app/src/main/java/com/omrobbie/mycheckboxapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.omrobbie.mycheckboxapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        boolean check = ((CheckBox) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.tea:
+                textView.setText("Tea");
+                break;
+
+            case R.id.milk:
+                textView.setText("Milk");
+                break;
+        }
     }
 }
