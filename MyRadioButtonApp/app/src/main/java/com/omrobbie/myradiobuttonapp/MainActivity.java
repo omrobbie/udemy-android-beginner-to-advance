20c8f76666defad48f2a17c668778ea5703a1ca4
omrobbie <putu.roby@yahoo.com>
2018-01-10 00:35:34 +0800

    setup radio button in action

--- MyRadioButtonApp/app/src/main/java/com/omrobbie/myradiobuttonapp/MainActivity.java
+++ MyRadioButtonApp/app/src/main/java/com/omrobbie/myradiobuttonapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.omrobbie.myradiobuttonapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClicked(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        int id = radioGroup.getCheckedRadioButtonId();
+
+        switch (id) {
+            case R.id.male:
+                textView.setText("Male");
+                break;
+
+            case R.id.female:
+                textView.setText("Female");
+                break;
+        }
     }
 }
