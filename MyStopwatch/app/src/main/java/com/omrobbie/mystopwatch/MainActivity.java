e3dcedcb842a0fdb93d7d4e5409be662a6f85199
omrobbie <putu.roby@yahoo.com>
2018-01-09 08:02:59 +0800

    create button listener method

--- MyStopwatch/app/src/main/java/com/omrobbie/mystopwatch/MainActivity.java
+++ MyStopwatch/app/src/main/java/com/omrobbie/mystopwatch/MainActivity.java
@@ -2,12 +2,45 @@ package com.omrobbie.mystopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    private TextView timeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeView = (TextView) findViewById(R.id.timeView);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+        String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+        timeView.setText(time);
+
+        if (running) seconds++;
     }
 }
