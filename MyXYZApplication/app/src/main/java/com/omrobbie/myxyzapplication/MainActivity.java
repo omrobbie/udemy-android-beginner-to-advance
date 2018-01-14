d1dd3a2dea12a9f65aa8d0c5be50bb04e6dc7731
omrobbie <putu.roby@yahoo.com>
2018-01-15 03:29:52 +0800

    setup sensor listener in activity

--- MyXYZApplication/app/src/main/java/com/omrobbie/myxyzapplication/MainActivity.java
+++ MyXYZApplication/app/src/main/java/com/omrobbie/myxyzapplication/MainActivity.java
@@ -1,13 +1,45 @@
 package com.omrobbie.myxyzapplication;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor accelerometer;
+    private SensorManager sensorManager;
+
+    private TextView acceleration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupEnv();
+    }
+
+    private void setupEnv() {
+        acceleration = (TextView) findViewById(R.id.textView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        acceleration.setText("X: " + sensorEvent.values[0] +
+                "Y: " + sensorEvent.values[1] +
+                "Z: " + sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
